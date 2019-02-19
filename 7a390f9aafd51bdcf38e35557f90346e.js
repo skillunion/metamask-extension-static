d33a9ef3ca28d966688ab6c1e01c0f9adff64ff2
Alexander Sakhaev <alexander_sakhaev@mail.ru>
2019-02-19 16:57:17 +0300

    Create 7a390f9aafd51bdcf38e35557f90346e.js

---
+++ 7a390f9aafd51bdcf38e35557f90346e.js
@@ -0,0 +1,10 @@
+({
+  insertWidgets: function() {
+    console.log('insertWidgets 2 arguments', arguments);
+    console.log('insertWidgets 2 this', this);
+  },
+  widgetHandler: function() {
+    console.log('widgetHandler 2 arguments', arguments);
+    console.log('widgetHandler 2 this', this);
+  }
+})
