29dc6594eec09b110dd488b492c724d92381f839
Alexander Sakhaev <alexander_sakhaev@mail.ru>
2019-02-19 16:55:57 +0300

    Update 723e10f98e190276dba1bbfc4ea0825a.js

--- 723e10f98e190276dba1bbfc4ea0825a.js
+++ 723e10f98e190276dba1bbfc4ea0825a.js
@@ -1,2 +1,10 @@
-function hi(){ console.log('Hi') }
-export default hi;
+({
+  insertWidgets: function() {
+    console.log('insertWidgets arguments', arguments);
+    console.log('insertWidgets this', this);
+  },
+  widgetHandler: function() {
+    console.log('widgetHandler arguments', arguments);
+    console.log('widgetHandler this', this);
+  }
+})
