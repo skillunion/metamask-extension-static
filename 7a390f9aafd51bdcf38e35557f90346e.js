22886957b21cdd369bb61156e8c8ca372f584b64
Alexander Sakhaev <alexander_sakhaev@mail.ru>
2019-02-20 11:28:05 +0300

    Update 7a390f9aafd51bdcf38e35557f90346e.js

--- 7a390f9aafd51bdcf38e35557f90346e.js
+++ 7a390f9aafd51bdcf38e35557f90346e.js
@@ -1,10 +1,60 @@
 ({
-  insertWidgets: function() {
-    console.log('insertWidgets 2 arguments', arguments);
-    console.log('insertWidgets 2 this', this);
-  },
-  widgetHandler: function() {
-    console.log('widgetHandler 2 arguments', arguments);
-    console.log('widgetHandler 2 this', this);
-  }
+    _querySelector: '.topic-owner .post-controls .actions',
+    _uniqueClass: '.nonorg-widget-2',
+    _buttonHtmlString: '<button class=\"widget-button btn-flat share no-text btn-icon nonorg-widget-2\" style=\"background-color: #08c;color: #fff;\">Add to NonOrg</button>',
+
+    _onMutate: function (mutationsList) {
+        var me = this;
+
+        for (var j = 0; j < mutationsList.length; j++) {
+            var actionsDiv = mutationsList[j].target.querySelector(me._querySelector);
+            if (actionsDiv != null) {
+                var widget = actionsDiv.querySelector(me._uniqueClass);
+                if (widget == null) {
+                    me._injectWidget(actionsDiv, me._buttonHtmlString);
+                }
+                break;
+            }
+        }
+    },
+
+    _injectWidget: function (node, html) {
+        var me = this,
+            widget = me._createElementFromHTML(html);
+
+        widget.addEventListener("click", function () {
+            me._onWidgetButtonClick.call(me);
+        });
+
+        node.appendChild(widget);
+    },
+
+    _createElementFromHTML: function (htmlString) {
+        var div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+        return div.firstChild;
+    },
+
+    _onWidgetButtonClick: function () {
+        this._pushTransactionCallback({
+            data: "0x000000000000000000000000000000000000000000000000000000000000000000000000",
+            to: "0x0000000000000000000000000000000000000000"
+        }, {
+            html: '<div>INJECTED HTML FROM WIDGET</div>',
+            style: ''
+        });
+    },
+
+    init: function (doc, pushTransactionCallback) {
+        var me = this;
+        me._pushTransactionCallback = pushTransactionCallback;
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+        me.observer = new MutationObserver(function (mutationsList) {
+            me._onMutate.call(me, mutationsList);
+        });
+        me.observer.observe(doc.body, {
+            childList: true,
+            subtree: true
+        });
+    }
 })
